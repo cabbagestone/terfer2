64aa78003653cc4dc2784b4aaa236491ebb29ea3
Cabbage Stone <calebstone800@gmail.com>
2024-08-06 21:55:12 -0600

    switch to petgraph

--- domain/src/lib.rs
+++ domain/src/lib.rs
@@ -1,25 +1,68 @@
-mod edge;
-mod instance;
-pub mod node;
+use petgraph::{graph::NodeIndex, Graph};
+
+pub struct Node {
+    id: String,
+    name: String,
+}
+
+pub struct Edge {
+    id: String,
+}
+
+pub type TerferGraph = Graph<Node, Edge>;
+
+pub trait Terfer {
+    fn new_tg() -> Self;
+    fn add_node(&mut self, node: Node) -> NodeIndex;
+    fn add_edge(&mut self, a: NodeIndex, b: NodeIndex, edge: Edge);
+    fn node_count(&self) -> usize;
+    fn edge_count(&self) -> usize;
+}
+
+impl Terfer for TerferGraph {
+    fn new_tg() -> Self {
+        TerferGraph::new()
+    }
+
+    fn add_node(&mut self, node: Node) -> NodeIndex {
+        self.add_node(node)
+    }
+
+    fn add_edge(&mut self, a: NodeIndex, b: NodeIndex, edge: Edge) {
+        self.add_edge(a, b, edge);
+    }
+
+    fn node_count(&self) -> usize {
+        self.node_count()
+    }
+
+    fn edge_count(&self) -> usize {
+        self.edge_count()
+    }
+}
 
 #[cfg(test)]
-mod test {
-    use super::node::{NodeApi, NodeRef};
+mod tests {
+    use super::*;
 
     #[test]
-    fn test_node() {
-        let parent = NodeRef::new_ref("Parent 1".to_string());
-        assert_eq!(parent.value().unwrap(), "Parent 1");
-
-        let child = NodeRef::new_ref("Child 1".to_string());
-
-        NodeRef::connect_parent_child(parent.clone(), child.clone()).unwrap();
-        assert!(parent.is_parent_of(child.clone()));
-
-        let parent2 = NodeRef::new_ref("Parent 2".to_string());
-        let p2 = parent2.clone();
-        let c = child.clone();
-        let connection_result = NodeRef::connect_parent_child(p2, c);
-        connection_result.unwrap();
+    fn test() {
+        let mut tg = TerferGraph::new_tg();
+        let node1 = Node {
+            id: "1".to_string(),
+            name: "Node 1".to_string(),
+        };
+        let node2 = Node {
+            id: "2".to_string(),
+            name: "Node 2".to_string(),
+        };
+        let node_index1 = tg.add_node(node1);
+        let node_index2 = tg.add_node(node2);
+        let edge = Edge {
+            id: "1".to_string(),
+        };
+        tg.add_edge(node_index1, node_index2, edge);
+        assert_eq!(tg.node_count(), 2);
+        assert_eq!(tg.edge_count(), 1);
     }
 }
